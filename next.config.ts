05c836d78b513fd5016d89f489af59503f93a8fd
tobias ferreira <soutobias@yahoo.com.br>
2026-05-28 14:14:55 +0100

    Update url

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from 'next';
 
 const isProd = process.env.NODE_ENV === 'production';
 
-const basePath = isProd ? '/breakingtheice' : '';
+// const basePath = isProd ? '/breakingtheice' : '';
+const basePath = isProd ? '' : '';
 
 const nextConfig: NextConfig = {
   /* config options here */
